d8ec16257df1438920101d18f1ae8eb8eddb8380
pigchilde <pigchilde@gmail.com>
2017-04-20 19:17:12 +0800

    接口请求功能制作

--- src/common/config/db.js
+++ src/common/config/db.js
@@ -7,16 +7,13 @@ export default {
   type: 'mysql',
   adapter: {
     mysql: {
-      host: '127.0.0.1',
-      port: '',
-      database: '',
-      user: '',
-      password: '',
+      host: 'localhost',
+      port: '3306',
+      database: 'freelance',
+      user: 'root',
+      password: 'root',
       prefix: '',
       encoding: 'utf8'
-    },
-    mongo: {
-
     }
   }
 };
--- src/home/controller/order.js
+++ src/home/controller/order.js
@@ -17,6 +17,23 @@ export default class extends think.controller.rest {
    * postAction
    */
   async postAction() {
-    return this.success({msg:'success!!'});
+    if(this.isPost()) {
+      let data = this.post();
+
+      let isAdd = await this.model('order').where({telephone:data.telephone}).find();
+
+      //判断用户是否提交过
+      if(!think.isEmpty(isAdd)) {
+         return this.fail("该用户已经提交过信息了！");
+      }
+
+      let result = this.model('order').add(data);
+
+      if(result){
+        return this.success(result);;
+      }else {
+        return this.fail("添加失败！")
+      }
+    }
   }
 }
